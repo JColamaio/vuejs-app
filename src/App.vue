5b1a147e9bd9f07de0cd13eeadd7379bbe79f876
JColamaio <jcolamaio@gmail.com>
2022-06-01 20:42:46 -0300

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,134 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+<div id="app">
+<main>
+  <div class="buscador">
+    <input type="text" class="buscadorcito" placeholder="Search..." />
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="weather-wrap">
+      <div class="location-box">
+        <div class="location"> Buenos Aires, Arg</div>
+        <div class="date"> Lunes 6 junio 2022</div>
+</div>
+      <div class="weather-box">
+        <div class="temperature">9°c </div>
+        <div class="weather">Clear</div>
+       </div>
+    </div> 
+</main>
+</div>
 </template>
 
+
+
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      api_key: '6a49ff79919f709add61a42ef87a1fca'
+    }
+  }
+  
+}
+</script>
+
+
+
+
+
 <style>
-@import './assets/base.css';
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
+body {
+  font-family: 'roboto';
+}
 #app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
+  background-image: url('./assets/cold.jpg');
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.5s;
 }
 
-header {
-  line-height: 1.5;
-}
+main {
+  min-height: 100vh;
+  padding: 25px;
 
-.logo {
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25),rgba(0, 0, 0, 0.75));
+}
+.buscador {
+  width: 100%;
+  margin-bottom: 30px;
+}
+.buscadorcito , .buscador {
   display: block;
-  margin: 0 auto 2rem;
-}
+  width: 100%;
+  padding: 15px;
+  color: #313131;
+  font-size: 20px;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
 
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
+box-shadow: 0px 0px 15px rgba(0, 0 , 0 ,0.25);
+  background-color: rgba(255, 255, 255 0.5);
+  border-radius: 0px 15px 0px 15px;
   transition: 0.4s;
 }
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
+.buscadorcito , .buscador:focus{
+  box-shadow: 0px 0px 15px rgba(0, 0 , 0 ,0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 15px 0px 15px 0px;
 }
 
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
+.location-box .location{
+  color: #FFF;
+  font-size: 30px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
 
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
+.location-box .date{
+  color: #FFF;
+  font-size: 20px;
+  font-weight: 300;
+  text-align: center;
+  font-style: italic;
+  
+}
 
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.weather-box {
+  text-align: center;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.weather-box .temperature {
+  display: inline-block;
+  padding: 10px 25px;
+  color: #FFF;
+  font-size: 100px;
+  font-weight: 900;
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+  text-shadow: 3px 6px rgba(0, 0 , 0 ,0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box .weather{
+  color: #FFF;
+  font-size: 45px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0 , 0 ,0.25);
 }
 </style>
